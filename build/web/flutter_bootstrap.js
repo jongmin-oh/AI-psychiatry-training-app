4f46869f4be234acc1f908ac5e84cb7ab4060471
jongmin-oh <alswhddh@naver.com>
2026-02-17 15:59:14 +0900

    Remove obsolete build artifacts and update service worker configuration
    - Deleted unnecessary build files including .last_build_id, favicon.png, flutter_service_worker.js, index.html, and various asset files to clean up the project.
    - Updated service worker version in flutter_bootstrap.js to ensure the latest configuration is used.
    - Added .build/ and /build/ to .gitignore to prevent future build artifacts from being tracked.

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2080590382"
+    serviceWorkerVersion: "2350538140"
   }
 });
